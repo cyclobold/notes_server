764547a9c483616bea9bf256df6543ed7ac4a24c
Olu <theoafactor@gmail.com>
2022-03-30 13:03:11 +0100

    Added Toasts and Mdals to frontend

--- index.js
+++ index.js
@@ -1,65 +1,62 @@
 const express = require("express");
 const mongodb = require("mongodb");
 const cors = require("cors");
-const server = express();
+const server = express(); //creates tthe server app
 
-const mongoClient = mongodb.MongoClient; 
+server.use(cors());
+server.use(express.json());
 
+//Create a MongoClient
+mongoClient = mongodb.MongoClient;
 const DB_URL = "mongodb+srv://cyclobold_user:e6b5eBt.$5PAcgx@cluster0.qcoqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-let connection = new mongoClient(DB_URL);
-
-
-
-//Middleware
-server.use(express.json());
-server.use(cors());
-
-//Port
-const PORT = 4000;
+const _conn = new mongoClient(DB_URL);
 
 //Endpoints
-
-//Get All Posts
 server.get("/get-posts", function(request, response){
-
-})
-
-//Create Posts
-server.post("/create-post", function(request, response){
-    post_title = request.body.postTitle;
-
-    post_message = request.body.postContent;
-
-    post_summary = request.body.postSummary;
-
-
-    //Submit the data to mongodb
-    connection.connect().then(() => {
-
-        connection.db("test_posts").collection("posts").insertOne({
-            post_title: post_title,
-            post_message: post_message,
-            post_summary: post_summary
-        })
-
-        response.send({
-            code: "success",
-            message: "Post created successfully",
-            data: request.body
-        })
-
-    })
-
-   
-  
     
 
+})
+
+
+server.post("/create-post", async function(request, response){
+    const post_title = request.body.postTitle;
+    const post_summary = request.body.postSummary;
+    const post_content = request.body.postContent;
+
+
+    //connect
+    feedback = await _conn.connect();
+
+    if(feedback){
+        feedback = await _conn.db("test_posts").collection("posts").insertOne({
+            post_title: post_title,
+            post_summary: post_summary,
+            post_content: post_content
+        });
+
+        if(feedback){
+            response.send({
+                message: "Post saved successfully",
+                code: "success"
+            })
+        }
+
+        
+
+
+
+    }
+
+
+
+
 
 })
 
 
 
 
-
-server.listen(PORT, () => console.log("Listening on PORT ", PORT));
+server.listen("4000", function(){
+    console.log("Server is listening ...")
+})
