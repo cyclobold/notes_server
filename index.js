e1697428e448f46eb3c18407bef29d6e505feb7b
Olu <theoafactor@gmail.com>
2022-04-01 11:58:43 +0100

    Added Get Posts

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongodb = require("mongodb");
+const dotenv = require("dotenv");
 const cors = require("cors");
+
+dotenv.config(); //load dotenv
+
+
 const server = express(); //creates tthe server app
 
 server.use(cors());
@@ -8,13 +13,25 @@ server.use(express.json());
 
 //Create a MongoClient
 mongoClient = mongodb.MongoClient;
-const DB_URL = "mongodb+srv://cyclobold_user:e6b5eBt.$5PAcgx@cluster0.qcoqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const DB_URL = process.env.DB_HOST_URL
 
 const _conn = new mongoClient(DB_URL);
 
 //Endpoints
-server.get("/get-posts", function(request, response){
-    
+server.get("/get-posts", async function(request, response){
+   feedback = await _conn.connect();
+
+   if(feedback){
+        result = await _conn.db(process.env.DB_NAME).collection("posts").find().toArray();
+        if(result){
+            response.send({
+                message: "Data retrieved...",
+                data: result,
+                code: "success"
+            });
+
+        }
+   }
 
 })
 
@@ -29,7 +46,7 @@ server.post("/create-post", async function(request, response){
     feedback = await _conn.connect();
 
     if(feedback){
-        feedback = await _conn.db("test_posts").collection("posts").insertOne({
+        feedback = await _conn.db(process.env.DB_NAME).collection("posts").insertOne({
             post_title: post_title,
             post_summary: post_summary,
             post_content: post_content
@@ -39,7 +56,7 @@ server.post("/create-post", async function(request, response){
             response.send({
                 message: "Post saved successfully",
                 code: "success"
-            })
+            }) 
         }
 
         
@@ -57,6 +74,6 @@ server.post("/create-post", async function(request, response){
 
 
 
-server.listen("4000", function(){
+server.listen(process.env.PORT, function(){
     console.log("Server is listening ...")
 })
